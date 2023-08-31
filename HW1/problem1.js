d82b5d4f293887cb5179842d637b359d4c26e084
npinnello <npin@iastate.edu>
2023-08-31 12:34:15 -0500

    most problem1

--- HW1/problem1.js
+++ HW1/problem1.js
@@ -3,35 +3,14 @@
 // updated in each frame (see the bottom of the main method) to animate
 // the model.
 
-
-// vertex shader
-const vshaderSource = `
-precision mediump float;
-attribute vec4 a_Position;
-uniform float shift;
-void main()
-{
-  // constructs a vec4 from a float and a vec3
-  gl_Position = vec4(a_Position.x + shift, a_Position.yzw);
-}
-`;
-
-// fragment shader
-const fshaderSource = `
-void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-  }
-`;
-
 // Raw data for some point positions - this will be a square, consisting
 // of two triangles.  We provide two values per vertex for the x and y coordinates
 // (z will be zero by default).
 var numPoints = 3;
 var vertices = new Float32Array([
--0.1, 0.1,
-0.5, -0.1,
+-0.5, -0.5,
+0.5, -0.5,
 0.5, 0.5,
-
 
 ]
 );
@@ -49,7 +28,7 @@ var vertexbuffer;
 var shader;
 
 // code to actually render our geometry
-function draw(shiftValue)
+function draw(xShiftValue, yShiftValue, scale)
 {
   // clear the framebuffer
   gl.clear(gl.COLOR_BUFFER_BIT);
@@ -70,13 +49,27 @@ function draw(shiftValue)
   // "enable" the a_position attribute
   gl.enableVertexAttribArray(positionIndex);
 
+  // associate the data in the currently bound buffer with the a_position attribute
+  // (The '2' specifies there are 2 floats per vertex in the buffer.  Don't worry about
+  // the last three args just yet.)
   gl.vertexAttribPointer(positionIndex, 2, gl.FLOAT, false, 0, 0);
 
   // we can unbind the buffer now (not really necessary when there is only one buffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  let index = gl.getUniformLocation(shader, "shift");
-  gl.uniform1f(index, shiftValue);
+  const scaleMatrix = new Float32Array([
+    scale, 0, 0, 0,
+    0, scale, 0, 0,
+    0, 0, scale, 0,
+    0, 0, 0, 1
+  ]);
+
+  const xIndex = gl.getUniformLocation(shader, "xShift");
+  const yIndex = gl.getUniformLocation(shader, "yShift");
+  const scaleIndex = gl.getUniformLocation(shader, 'scaleMatrix');
+  gl.uniform1f(xIndex, xShiftValue);
+  gl.uniform1f(yIndex, yShiftValue);
+  gl.uniformMatrix4fv(scaleIndex, false, scaleMatrix);
 
   // draw, specifying the type of primitive to assemble from the vertices
   gl.drawArrays(gl.TRIANGLES, 0, numPoints);
@@ -91,6 +84,8 @@ function draw(shiftValue)
 // entry point when page is loaded
 function main() {
 
+
+  
   // basically this function does setup that "should" only have to be done once,
   // while draw() does things that have to be repeated each time the canvas is
   // redrawn
@@ -99,7 +94,7 @@ function main() {
   gl = getGraphicsContext("theCanvas");
 
   // load and compile the shader pair
-  shader = createShaderProgram(gl, vshaderSource, fshaderSource);
+  shader = createProgram(gl, 'vertexShader', 'fragmentShader');
 
   // load the vertex data into GPU memory
   vertexbuffer = createAndLoadBuffer(vertices);
@@ -112,14 +107,18 @@ function main() {
   // shaders
   //draw();
 
-  let shift = -0.2;
-  let increment = 0.01;
+  let theta = 0;
+  const r = 0.75;
+  const increment = toRadians(1);
 
   // define an animation loop
   var animate = function() {
-  	draw(shift);
-    if (shift < -0.2 || shift > 0.5) increment = -increment;
-    shift += increment;
+    const x = r * Math.cos(theta)
+    const y = r * Math.sin(theta)
+    const scale = parseFloat(document.getElementById('scaleBox').value)
+  	draw(x, y, scale);
+    if (toDegrees(theta) > 360) theta = 0
+    else theta += increment
 
   	// request that the browser calls animate() again "as soon as it can"
       requestAnimationFrame(animate);
