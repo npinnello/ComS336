c7cae46d073f98790ae86eafeb627be1e2e9bf7d
npinnello <npin@iastate.edu>
2023-09-06 22:06:06 -0500

    triangle prob4

--- HW1/problem4.js
+++ HW1/problem4.js
@@ -28,10 +28,10 @@ void main()
 
 // raw data for some point positions
 const vertices = new Float32Array([
-  -1.0, -1.0,
-  1.0, -1.0,
-  -1.0, 1.0,
-  1.0, 1.0,
+  -0.5, -0.75,
+  0.5, -0.75,
+  0.0, 1.0,
+  -0.5, 
 ])
 
 // a color value for each vertex
